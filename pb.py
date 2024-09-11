8ba306778201086dcd11b2913e5322a4eedc36a3
unknown <fazlerabbi3510@gmail.com>
2024-09-11 20:48:53 +0800

    practice some python 11-sep

--- pb.py
+++ pb.py
@@ -128,3 +128,248 @@ def jogfol(songka):
 
     return total
 print(jogfol((8, 43, 2, 7)))"""
+
+
+
+
+## 3  multipication
+
+"""def multply (numbers):
+    total = 1
+
+    for k in numbers:
+        total *= k
+    return total
+print(multply((4, 8, 2, 1, 10)))"""
+
+"""
+def gunfall(songka):
+    hati = 1
+    for h in songka:
+        hati *= h
+    return(hati)
+print(gunfall((2, 4, 5, 5, 9)))
+"""
+
+
+
+
+
+### 4 Reverse a STRING
+"""
+def string_reverse(str1):
+    retr1 = ''
+
+    index = len(str1)
+
+    while index > 0:
+        retr1 += str1[index -1]
+
+        index = index - 1
+
+    return retr1"""
+
+
+
+
+
+## 5  Number in a range
+"""
+def test_range(n):
+    if n in range(3, 9):
+        print("%s is in the range " %str(n))
+    else:
+        print("The number is outside the given range")
+
+test_range(5)"""
+
+
+
+## 7 Calculate the Number
+"""
+def string_test(s):
+    d = {"UPPER_CASE": 0, "LOWER_CASE": 0}
+
+    for c in s:
+        if c.isupper():
+            d["UPPER_CASE"] += 1
+        elif c.islower():
+            d["LOWER_CASE"] += 1
+        else:
+            pass
+
+    print("Original String: ", s)
+
+    print("No, of Upper case characters: ", d["UPPER_CASE"])
+
+    print("No. of lower case Characters: ", d["LOWER_CASE"])
+string_test("Fazle Rabbi")
+
+# another one
+def string(s):
+    a = 0
+    b = 0
+
+    for c in s:
+        if c.isupper():
+            a += 1
+        elif c.islower():
+            b += 1
+        else:
+            pass
+    
+    print("Original String: ", s)
+    print("No of upper case: ", a)
+    print("No of lower case: ", b)
+string("My name is MD Fazle Rabbi")"""
+
+
+
+####  8 takes a list and returns a new distinct list
+"""
+def list_1(l):
+    x = []
+
+    for k in l:
+        if k not in x:
+            x.append(k)
+
+    return x
+print(list_1([1,2,4, 8, 4, 3,5,4])
+
+## Another one
+
+def my_fun(x_list):
+    y_list = []
+
+    for x in x_list:
+        if x not in y_list:
+            y_list.append(x)
+    return y_list
+print(my_fun([1,1,1,2,3,4,4,4,5,6,5,5,9]))"""
+
+
+
+### 9 it's pirme or not
+"""def test_prime(n):
+
+    if n == 1:
+        return False
+    elif n == 2:
+        return True
+    else:
+        for x in range(2, n):
+            if n % x == 0:
+                return False
+        return True
+print(test_prime(9))
+
+
+from math import sqrt
+def is_prime(num):
+    if num <= 1 or (num % 2 == 0 and num > 2):
+        return False
+    return all (num % i for i in range(3, int(sqrt(num)) +1, 2))
+
+
+print(is_prime(11))
+print(is_prime(18))"""
+
+
+"""
+## 10 Find a even number list
+def even_test(n):
+    e_lsit = []
+
+    for k in n:
+        if k % 2 == 0:
+            e_lsit.append(k)
+    return e_lsit
+print(even_test([1,2,4,5,4, 9, 3]))"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## 12 Wheter a passed string is a palindrome or not
+"""
+def is_palindrome(string):
+    left_pass = 0
+    right_pass = len(string) -1
+
+    while right_pass >= left_pass:
+        if not string[left_pass] == string[right_pass]:
+            return False
+        
+        left_pass += 1
+        right_pass -= 1
+
+    return True
+
+print(is_palindrome('madamimadam'))"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### 13 print a list where the number is in the square list
+"""
+def printValues():
+    l = list()
+    for i in range(1,21):
+        l.append(i**2)
+    print(l)
+printValues()"""
+
+
+# 14  print a list where the number is in the square root list
+"""
+def R_root():
+    m = list(range(1, 60))
+    l = list()
+
+    for k in m:
+        for i in m:
+            if (k**0.5)==i:
+                l.append(i)
+    print(l)
+R_root()"""
+
+
+## 15 
+"""
+
+def test(a):
+    def add(b):
+        nonlocal a
+        a += 6
+        
+        return a+b
+    return add
+func = test(4)
+print(func(4))"""
+
--- pp_2.py
+++ pp_2.py
@@ -47,11 +47,273 @@ while turns > 0:
 
 
 
-
+"""
 def sum(numbers):
     total = 0
 
     for k in numbers:
         total += k
     return total
-print(sum((34, 2, 4, 20, 17, 13)))
+print(sum((34, 2, 4, 20, 17, 13)))"""
+
+"""def sum(*numbers):
+       total = 0
+        for i in numbers:
+            total += i
+        return total
+print(sum(3, 5, 9, 2))
+def multiply(numbers):
+    multi = 1
+    for k in numbers:
+        multi *= k 
+    return multi"""
+
+
+"""
+def r_string(string):
+
+    r_str = ''
+
+    index = len(string)
+
+    while index > 0:
+        r_str += string[index -1]
+
+        index = index -1
+    return r_str
+print(r_string("abcd"))
+
+
+
+
+def my_fun(string):
+    string_2 = ''
+    index = len(string)
+
+    while index > 0:
+        string_2 += string[index -1]
+        index = index -1
+    return string_2
+print(my_fun("Rabbi"))
+"""
+
+
+"""
+##### 4
+def reverse_str(string):
+    r_str = ''
+    index = len(string)
+
+    while index > 0:
+        r_str += string[index -1]
+
+        index = index -1
+    return r_str
+print(reverse_str("MyName"))
+
+
+def name(apple):
+    r_name = ''
+
+    counter = len(apple)
+
+    while counter > 0:
+        r_name += apple[counter -1]
+        counter = counter -1
+    return r_name
+print(name("APPLE"))"""
+
+"""
+def multiply(numbers):
+    total = 1
+
+    for k in numbers:
+        total *= k
+    return total
+print(multiply((12, 2, 5, 3, 5 )))
+
+
+
+def sum(numbers):
+    total = 0
+    for s in numbers:
+        total += s
+
+    return total
+print(sum((12, 5, 13, 5, 15)))
+
+
+def max_finder(numbers):
+    total = 0
+    for k in numbers:
+        return k
+    
+print(max_finder((12, 4, 35, 94)))
+
+ """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def test_range(n):
+    if n in (3 ,9):
+        print("%s in the range" %str(n))
+    else:
+        print("%s The number is not in that list" %str(n))
+test_range(96)
+
+
+def test_range(k):
+    if k in range(1, 20):
+        print("%s in the range" %str(k))
+    else:
+        print("%s is not on that list" %str(k))
+test_range(121)
+"""
+
+
+
+
+"""def string(s):
+    a = 0
+    b = 0
+
+    for c in s:
+        if c.isupper():
+            a += 1
+        elif c.islower():
+            b += 1
+        else:
+            pass
+    
+    print("Original String: ", s)
+    print("No of upper case: ", a)
+    print("No of lower case: ", b)
+string("My name is MD Fazle Rabbi")"""
+
+
+
+"""
+def x_list(list_s):
+    list_n = []
+
+    for a in list_s:
+        if a not in list_n:
+            list_n.append(a)
+    return list_n
+print(x_list([1,2,5,5,2]))
+
+
+
+def my_fun(x_list):
+    y_list = []
+
+    for x in x_list:
+        if x not in y_list:
+            y_list.append(x)
+    return y_list
+print(my_fun([1,1,1,2,3,4,4,4,5,6,5,5,9]))"""
+
+"""
+def test_prime(n):
+    if n == 1:
+        return False
+    elif n == 2:
+        return True
+    else:
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+print(test_prime(1))
+            
+
+
+from math import sqrt
+
+def is_prime(num):
+    if num <=1 or (num % 2 == 0 and num >2):
+        return False
+    return all (num % i for i in range(3, int(sqrt(num)) +1, 2))
+
+print(is_prime(229))
+
+
+from math import sqrt
+
+def is_prime(n):
+    if n <= 1 or (n > 2 and n % 2 == 0):
+        return False
+    return all( n % k for k in range(3, int(sqrt(n)) +1, 2))
+
+print(is_prime(12))
+
+"""
+
+
+"""
+
+def values():
+    l = list()
+
+    for i in range(1, 10):
+        l.append(i**3)
+    print(l)
+values()
+"""
+"""
+
+def root():
+    m = list(range(1,10))
+    r = list()
+
+    for k in m:
+        
+root()
+"""
+
+### it will give you a range of certain number square root form
+
+
+""" 
+m = list(range(1,40))
+l = list()
+for i in m:
+    for k in m:
+        if (k**0.5) == i:
+            l.append(i)
+            print(l)"""
+
+def R_root():
+    m = list(range(1, 60))
+    l = list()
+
+    for k in m:
+        for i in m:
+            if (k**0.5)==i:
+                l.append(i)
+    print(l)
+R_root()
+
+
+
+
+
+
+"""
+numbers = [12, 4, 35, 94]
+for k in numbers:
+    print(k)"""
+   
